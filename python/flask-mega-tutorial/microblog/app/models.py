7611771417e4bee80fe2357da3555cd6f7a37902
Cool BRG <budhram.gurung01@gmail.com>
2019-12-07 22:59:38 +0545

    Flask mega tutorial: followers

--- python/flask-mega-tutorial/microblog/app/models.py
+++ python/flask-mega-tutorial/microblog/app/models.py
@@ -4,6 +4,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+followers = db.Table('followers',
+                    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                )
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -13,6 +18,13 @@ class User(UserMixin, db.Model):
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+    )
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -26,6 +38,30 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id
+        ).count() > 0
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def followed_posts(self):
+        followed_post = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)
+        ).filter(followers.c.follower_id == self.id)
+
+        print("My followed Posts: ", followed_post)
+
+        own_posts = Post.query.filter_by(user_id = self.id)
+        return followed_post.union(own_posts).order_by(Post.timestamp.desc())
+
 
 
 @login.user_loader
--- python/flask-mega-tutorial/microblog/app/routes.py
+++ python/flask-mega-tutorial/microblog/app/routes.py
@@ -70,11 +70,10 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts=[
-        {'author': user, 'body': 'Test Post #1'},
-        {'author': user, 'body': 'Test Post #2'}
-    ]
 
+    print("User is : ", user.username)
+    posts = user.followed_posts()
+    print("Followed post are: ", posts)
     return render_template('user.html', user=user, posts=posts)
 
 @app.before_request
@@ -97,3 +96,34 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username = username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself')
+        return redirect(url_for('user', username=username))
+
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username))
+    return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username = username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following {}.'.format(username))
+    return redirect(url_for('user', username=username))
