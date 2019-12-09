412cc82b46010505759b97be5610fe96e470af12
Cool BRG <budhram.gurung01@gmail.com>
2019-12-09 21:54:07 +0545

    Flask mega tutorial: password reset & email support

--- python/flask-mega-tutorial/microblog/app/models.py
+++ python/flask-mega-tutorial/microblog/app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
-from app import db, login
+from app import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+from time import time
+import jwt
+
 
 followers = db.Table('followers',
                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -61,6 +64,22 @@ class User(UserMixin, db.Model):
 
         own_posts = Post.query.filter_by(user_id = self.id)
         return followed_post.union(own_posts).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {
+                'reset_password': self.id, 'exp': time() + expires_in
+            },
+            app.config['SECRET_KEY'], algorithm='HS256'
+        ).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 
 
